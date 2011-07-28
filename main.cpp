27007d684b33b56472f09bcd721eaa399ab2477c
BYVoid <byvoid.kcp@gmail.com>
2011-07-28 21:23:05 +0800

    Fix the bug of single port.

--- main.cpp
+++ main.cpp
@@ -7,11 +7,35 @@
 #include <QDir>
 #include <QVariantList>
 
-void startServer(quint16 port)
+void startInstance(quint16 port)
 {
     ServerThread * server_thread = new ServerThread();
     server_thread->setPort(port);
     server_thread->start();
+}
+
+bool start()
+{
+    QVariantList port_list;
+    bool ok;
+    QVariant t_port = Settings::instance().value("httpd/port").toUInt(&ok);
+    if (ok)
+        port_list.push_back(t_port);
+    else
+        port_list = t_port.toList();
+
+    if (port_list.size() == 0)
+    {
+        Log::instance() << "No port specified to listen." << Log::NEWLINE << Log::FLUSH;
+        return false;
+    }
+
+    for (QVariantList::iterator i = port_list.begin(); i != port_list.end(); ++i)
+    {
+        startInstance(i->toUInt());
+    }
+
+    return true;
 }
 
 int main(int argc, char *argv[])
@@ -21,17 +45,8 @@ int main(int argc, char *argv[])
 
     Log::instance() << APPLICATION_NANE << Log::NEWLINE << Log::FLUSH;
 
-    QVariantList port_list = Settings::instance().value("httpd/port").toList();
-    if (port_list.size() == 0)
-    {
-        Log::instance() << "No port specified to listen." << Log::NEWLINE << Log::FLUSH;
-        return 1;
-    }
-
-    for (QVariantList::iterator i = port_list.begin(); i != port_list.end(); ++i)
-    {
-        startServer(i->toUInt());
-    }
+    if (!start())
+        return -1;
 
     return a.exec();
 }
